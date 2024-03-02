201f2a13e5c4a79e32f6d750717299181c394956
bybang <joshuaBang26@gmail.com>
2024-03-03 00:04:06 +0900

    TIL update on 03 Mar 2024

--- 08_JS_Vanilla_Starbucks/starbucksDemo/js/main.js
+++ 08_JS_Vanilla_Starbucks/starbucksDemo/js/main.js
@@ -17,6 +17,52 @@ searchInputEl.addEventListener("blur", function () {
 
 const badgeEl = document.querySelector("header .badges");
 
-window.addEventListener("scroll", function () {
-  console.log("scroll!");
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.6,
+    opacity: 1,
+  });
+});
+
+new Swiper(".notice-line .swiper", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper(".promotion .swiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
 });
