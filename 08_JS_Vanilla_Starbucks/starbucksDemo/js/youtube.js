81a8f1f3fb8e45fd2d6abb2651f7267e9427f605
bybang <joshuaBang26@gmail.com>
2024-04-03 00:01:47 +0900

    TIL update on 03 Apr 2024

---
+++ 08_JS_Vanilla_Starbucks/starbucksDemo/js/youtube.js
@@ -0,0 +1,23 @@
+// This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// This function creates an <iframe> (and YouTube player) after the API code downloads.
+function onYouTubeIframeAPIReady() {
+  new YT.Player("player", {
+    videoId: "An6LvWQuj_8",
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playlist: "An6LvWQuj_8", // video in que for the loop
+    },
+    events: {
+      onReady: function (event) {
+        event.target.mute();
+      },
+    },
+  });
+}
